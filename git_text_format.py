c931a527f99df80c3b3fae21da24a92016c9a411
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-09 11:31:34 -0400

    Change 1

---
+++ git_text_format.py
@@ -0,0 +1,14 @@
+# Format
+
+first = "John"
+last = "Smith"
+message = f'{first} [{last}] is a coder'
+print(message)
+
+# String Methods
+
+course ='Python for Beginners'
+print(len(course))
+print(course.find('Beginners'))
+print(course.replace('Beginners', 'Newbies'))
+
