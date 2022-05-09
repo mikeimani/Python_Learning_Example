ac2dff867b85dd42bec50110430859330b040503
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-09 12:20:23 -0400

    change 1

--- git_text_format.py
+++ git_text_format.py
@@ -12,3 +12,20 @@ print(len(course))
 print(course.find('Beginners'))
 print(course.replace('Beginners', 'Newbies'))
 
+price = 1000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f'down_payment: ${down_payment}')
+
+weight = int(input("Weight: "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} Kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
