838388f271b0a3268832fc1125bfe990d51bd4fb
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-10 11:19:22 -0400

    change 1

--- git_text_loops.py
+++ git_text_loops.py
@@ -47,6 +47,13 @@ for x_count in numbers:
         output += "x"
     print(output)
 
+numbers = [3, 6, 2, 8, 4, 10]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
+
 
 
 
