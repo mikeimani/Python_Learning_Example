9ae36917abb9049f8f18d560ea89ac0bd62b718c
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-12 12:14:09 -0400

    change 1

--- sql table.py
+++ sql table.py
@@ -13,3 +13,15 @@ print(table_list[2])
 
 print(table_list[2][2])
 
+first_name = input('What is your first name? ')
+last_name = input('What is your last name?')
+city = input("What is your city? ")
+state = input("What is your state? ")
+
+sql_string = f"INSERT INTO owners(first_name, last_name, city, state) VALUES('{first_name}','{last_name}','{city}','{state}')"
+print(f'Your name is {first_name} {last_name} and you live in {city}, {state}')
+print(sql_string)
+cursor.execute(sql_string)
+connection.commit()
+cursor.execute("Select * From owners")
+print(cursor.fetchall())
