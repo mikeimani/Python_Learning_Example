cfff51b6678835e30d25605ce7e9528fe4a6ea76
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-13 09:47:12 -0400

    change 1

--- git_text_funct.py
+++ git_text_funct.py
@@ -9,3 +9,26 @@ print("Start")
 greet_user()
 print("Finish")
 
+
+
+def greet_user(name):
+    print(f"Hi {name}!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user('John')
+print("Finish")
+
+def square(number):
+    return number * number
+
+print(square(3))
+
+# Exceptions
+
+try:
+    age = int(input("Age: "))
+    print(age)
+except ValueError:
+    print("Invalide value")
