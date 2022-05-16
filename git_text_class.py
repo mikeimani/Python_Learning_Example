87d35a89b268b239a84d81b11c1fa03548311615
mikeimani <100438745+mikeimani@users.noreply.github.com>
2022-05-15 21:43:52 -0400

    change 1

--- git_text_class.py
+++ git_text_class.py
@@ -29,8 +29,7 @@ class Person:
     def __init__(self, name):
         self.name = name
     def talk(self):
-        print("talk")
+        print(f"Hi I am {self.name}")
 
 John = Person("John Smith")
-print(John.name)
 John.talk()
